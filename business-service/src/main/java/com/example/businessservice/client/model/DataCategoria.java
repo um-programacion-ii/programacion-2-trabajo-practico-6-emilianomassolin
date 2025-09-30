85cda2459ca00e72bfc686c0e8aefcad3e860851
emilianomassolin <e.massolin@alumno.um.edu.ar>
2025-09-30 17:47:12 -0300

    agrego model datacategoria

---
+++ business-service/src/main/java/com/example/businessservice/client/model/DataCategoria.java
@@ -0,0 +1,10 @@
+package com.example.businessservice.client.model;
+
+import lombok.Data;
+
+@Data
+public class DataCategoria {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+}
